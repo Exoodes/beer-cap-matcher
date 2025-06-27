58e964b4ae21b6d278761b1539e9e67489a42426
Exoodes <jakub.maca.01@gmail.com>
2025-06-27 14:21:49 +0200

    feat: add .gitignore

---
+++ src/main.py
@@ -0,0 +1,31 @@
+import argparse
+from dotenv import load_dotenv
+from src.preprocessing.image_processor import ImageAugmenter
+import os
+
+load_dotenv()
+
+
+def run_augmentation():
+    raw_data_dir = os.getenv("RAW_DATA_DIR")
+    augmented_data_dir = os.getenv("AUGMENTED_DATA_DIR")
+    if raw_data_dir is None or augmented_data_dir is None:
+        raise ValueError("RAW_DATA_DIR and AUGMENTED_DATA_DIR environment variables must be set.")
+
+    augmenter = ImageAugmenter(
+        input_dir=raw_data_dir,
+        output_dir=augmented_data_dir,
+        augmentations_per_image=int(os.getenv("AUGMENTATIONS_PER_IMAGE", 20)),
+    )
+    augmenter.augment_and_save()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Beer cap matcher CLI.")
+    parser.add_argument("--augment", action="store_true", help="Enable generation of augmented pictures")
+    parser.add_argument("--index", action="store_true", help="Enable indexing of pictures")
+    parser.add_argument("--update", action="store_true", help="Enable updating of the index")
+    args = parser.parse_args()
+
+    if args.augment:
+        run_augmentation()
