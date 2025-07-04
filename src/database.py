1e9ee1c92e40273c1b749d7ce6b3ddbdb08d533e
Exoodes <jakub.maca.01@gmail.com>
2025-07-04 16:00:29 +0200

    refactor: disable SQL echo in database engine for cleaner logs

--- src/database.py
+++ src/database.py
@@ -5,6 +5,6 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv("POSTGRES_DATABASE_URL")
 
-engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+engine = create_async_engine(DATABASE_URL, echo=False, future=True)
 
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
