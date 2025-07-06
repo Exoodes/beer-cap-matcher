f17b2f08a4116240dac4d747e25588958d003ae6
Exoodes <jakub.maca.01@gmail.com>
2025-07-06 15:47:05 +0200

    refactor: encapsulate database engine and session creation in get_db_resources function

--- src/database.py
+++ src/database.py
@@ -1,10 +1,19 @@
 import os
+from typing import Tuple
 
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("POSTGRES_DATABASE_URL")
 
-engine = create_async_engine(DATABASE_URL, echo=False, future=True)
+def get_db_resources(database_url: str, echo: bool = False) -> Tuple[AsyncEngine, sessionmaker]:
 
-async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+    engine = create_async_engine(database_url, echo=echo, future=True)
+    async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+    return engine, async_session
+
+
+GLOBAL_DATABASE_URL = os.getenv("POSTGRES_DATABASE_URL")
+if not GLOBAL_DATABASE_URL:
+    raise ValueError("POSTGRES_DATABASE_URL environment variable is not set.")
+
+GLOBAL_ENGINE, GLOBAL_ASYNC_SESSION_MAKER = get_db_resources(GLOBAL_DATABASE_URL)
