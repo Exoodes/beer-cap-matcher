6dcd0124d3e888b88e916f1709045633e6b5d14a
Exoodes <jakub.maca.01@gmail.com>
2025-07-06 15:44:48 +0200

    fix: update import statement for declarative_base in models initialization

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,9 @@
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
+
+from .augmented_cap import AugmentedCap
+from .beer import Beer
+from .beer_cap import BeerCap
+
+__all__ = ["Base", "Beer", "BeerCap", "AugmentedCap"]
