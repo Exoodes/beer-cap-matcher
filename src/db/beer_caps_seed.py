929a52d1a19ac0333a72f59fca08ea9244151566
Exoodes <jakub.maca.01@gmail.com>
2025-08-05 22:40:23 +0200

    feat: add initial beer cap data for Bernard, Staropramen, and Svijany

---
+++ src/db/beer_caps_seed.py
@@ -0,0 +1,25 @@
+# {"file_name": ".jpg", "beer_name": ""},
+
+data = {
+    "Bernard": [
+        {"file_name": "011.jpg", "beer_name": "Bernard Kvasnicové 10°"},
+        {"file_name": "032.jpg", "beer_name": "Bernard 12° Světlý ležák"},
+        {"file_name": "033.jpg", "beer_name": "Bernard 11°"},
+        {"file_name": "038.jpg", "beer_name": "Bernard Kvasnicové 11°"},
+        {"file_name": "045.jpg", "beer_name": "Bernard 10°"},
+        {"file_name": "086.jpg", "beer_name": "Bernard grep nealkoholické"},
+    ],
+    "Staropramen": [
+        {"file_name": "004.jpg", "beer_name": "Staropramen 11°"},
+        {"file_name": "018.jpg", "beer_name": "Staropramen 11°", "variant": "Staropramen třetinka"},
+        {"file_name": "056.jpg", "beer_name": "Staropramen 10°"},
+        {"file_name": "059.jpg", "beer_name": "Staropramen 11°", "variant": "Červené víčko - asi blbost"},
+        {"file_name": "061.jpg", "beer_name": "Staropramen 10° Déčko"},
+        {"file_name": "076.jpg", "beer_name": "Staropramen 12°"},
+    ],
+    "Svijany": [
+        {"file_name": "062.jpg", "beer_name": "Svijanský kníže"},
+        {"file_name": "067.jpg", "beer_name": "Svijanský Máz 11° světlý ležák", "variant": "Vánoční speciál"},
+        {"file_name": "084.jpg", "beer_name": "Svijanský Máz 11° světlý ležák"},
+    ],
+}
