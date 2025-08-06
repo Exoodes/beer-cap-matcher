d9750cc2306b48a16f8c108482ec7d25bda83f52
Jakub Maca <jakub.maca@chyron.com>
2025-08-06 08:34:33 +0200

    feat: restructure beer cap data to use nested dictionaries for better organization

--- src/db/beer_caps_seed.py
+++ src/db/beer_caps_seed.py
@@ -1,25 +1,29 @@
 # {"file_name": ".jpg", "beer_name": ""},
 
 data = {
-    "Bernard": [
-        {"file_name": "011.jpg", "beer_name": "Bernard Kvasnicové 10°"},
-        {"file_name": "032.jpg", "beer_name": "Bernard 12° Světlý ležák"},
-        {"file_name": "033.jpg", "beer_name": "Bernard 11°"},
-        {"file_name": "038.jpg", "beer_name": "Bernard Kvasnicové 11°"},
-        {"file_name": "045.jpg", "beer_name": "Bernard 10°"},
-        {"file_name": "086.jpg", "beer_name": "Bernard grep nealkoholické"},
-    ],
-    "Staropramen": [
-        {"file_name": "004.jpg", "beer_name": "Staropramen 11°"},
-        {"file_name": "018.jpg", "beer_name": "Staropramen 11°", "variant": "Staropramen třetinka"},
-        {"file_name": "056.jpg", "beer_name": "Staropramen 10°"},
-        {"file_name": "059.jpg", "beer_name": "Staropramen 11°", "variant": "Červené víčko - asi blbost"},
-        {"file_name": "061.jpg", "beer_name": "Staropramen 10° Déčko"},
-        {"file_name": "076.jpg", "beer_name": "Staropramen 12°"},
-    ],
-    "Svijany": [
-        {"file_name": "062.jpg", "beer_name": "Svijanský kníže"},
-        {"file_name": "067.jpg", "beer_name": "Svijanský Máz 11° světlý ležák", "variant": "Vánoční speciál"},
-        {"file_name": "084.jpg", "beer_name": "Svijanský Máz 11° světlý ležák"},
-    ],
+    "Bernard": {
+        "Bernard Kvasnicové 10°": [{"file_name": "011.jpg"}],
+        "Bernard 12° Světlý ležák": [{"file_name": "032.jpg"}],
+        "Bernard 11°": [{"file_name": "033.jpg"}],
+        "Bernard Kvasnicové 11°": [{"file_name": "038.jpg"}],
+        "Bernard 10°": [{"file_name": "045.jpg"}],
+        "Bernard grep nealkoholické": [{"file_name": "086.jpg"}],
+    },
+    "Staropramen": {
+        "Staropramen 11°": [
+            {"file_name": "004.jpg"},
+            {"file_name": "018.jpg", "variant": "Staropramen třetinka"},
+            {"file_name": "059.jpg", "variant": "Červené víčko - asi blbost"},
+        ],
+        "Staropramen 10°": [{"file_name": "056.jpg"}],
+        "Staropramen 10° Déčko": [{"file_name": "061.jpg"}],
+        "Staropramen 12°": [{"file_name": "076.jpg"}],
+    },
+    "Svijany": {
+        "Svijanský kníže": [{"file_name": "062.jpg"}],
+        "Svijanský Máz 11° světlý ležák": [
+            {"file_name": "067.jpg", "variant": "Vánoční speciál"},
+            {"file_name": "084.jpg"},
+        ],
+    },
 }
