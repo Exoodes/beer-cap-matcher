e70ec8d9bd28b8ec2b52d6bb0d6229e343d0f985
Exoodes <jakub.maca.01@gmail.com>
2026-07-13 19:42:04 +0200

    Add all images

--- src/db/beer_caps_seed.py
+++ src/db/beer_caps_seed.py
@@ -36,6 +36,7 @@ data = {
             ],
             "Radegast Hutná 15°": [{"file_name": "077.jpg"}],
             "Radegast Ratar": [{"file_name": "019.jpg"}],
+            "Radegast 12 Temně hořká": [{"file_name": "083.jpg"}],
         },
         "Kozel": {
             "Velkopopovický Kozel 11°": [{"file_name": "052.jpg"}],
@@ -44,5 +45,205 @@ data = {
             "Velkopopovický Kozel Mistrův ležák": [{"file_name": "082.jpg"}],
             "Velkopopovický Kozel řezaný": [{"file_name": "014.jpg"}],
         },
-    }
+        "Gambrinus": {
+            "Gambrinus 10°": [{"file_name": "003.jpg"}],
+            "Gambrinus 12°": [{"file_name": "007.jpg"}],
+            "Gambrinus Dry": [{"file_name": "025.jpg"}],
+            "Gambrinus 12° Patron": [{"file_name": "044.jpg"}],
+            "Gambrinus 10° Originál": [{"file_name": "079.jpg"}],
+        },
+        "Starobrno": {
+            "Starobrno Medium": [{"file_name": "010.jpg"}],
+            "Starobrno Bitr": [{"file_name": "016.jpg"}],
+            "Starobrno Tradiční": [{"file_name": "022.jpg"}],
+            "Starobrno Medium ležák": [{"file_name": "060.jpg"}],
+        },
+        "Krušovice": {
+            "Krušovice 12°": [{"file_name": "017.jpg"}],
+            "Krušovice Bohém": [{"file_name": "073.jpg"}],
+            "Krušovice 10°": [{"file_name": "085.jpg"}],
+        },
+        "Budějovický Budvar": {
+            "Budvar - Tmavý ležák": [{"file_name": "012.jpg"}],
+            "Budvar 33": [{"file_name": "027.jpg"}],
+            "Budvar": [{"file_name": "072.jpg"}],
+        },
+        "Plzeňský Prazdroj": {
+            "Plzeň": [{"file_name": "064.jpg"}],
+        },
+        "Primátor": {
+            "Primátor 11": [{"file_name": "029.jpg"}],
+            "Primátor Premium světlý ležák?": [{"file_name": "049.jpg"}],
+            "Primátor Weizen (Pšeničné)": [{"file_name": "050.jpg"}],
+        },
+        "Rychtář": {
+            "Rychtář Grunt 11": [{"file_name": "020.jpg"}],
+            "Rychtář": [{"file_name": "055.jpg"}],
+        },
+        "Platan": {
+            "Platan Granát": [{"file_name": "024.jpg"}],
+            "Platan 11": [{"file_name": "048.jpg"}],
+        },
+        "Ostravar": {
+            "Mustang 11°": [{"file_name": "008.jpg"}],
+            "Ostravar 12°": [{"file_name": "039.jpg"}],
+        },
+        "Pivovar Nová Paka": {
+            "Valdštejn - Pivovar nová paka": [{"file_name": "034.jpg"}],
+        },
+        "Pivovar Rohozec": {
+            "Rohozec Třináctka": [{"file_name": "071.jpg"}],
+        },
+        "Pivovar Nymburk": {
+            "Klasik": [{"file_name": "043.jpg"}],
+        },
+        "Pivovar ZUBR": {
+            "Zubr": [{"file_name": "036.jpg"}],
+        },
+        "Měšťanský pivovar Havlíčkův Brod": {
+            "Rebel klasický": [{"file_name": "030.jpg"}],
+            "Rebel India Pale Ale": [{"file_name": "089.jpg"}],
+        },
+        "Lobkowicz": {
+            "Lobkowicz Nealko": [{"file_name": "035.jpg"}],
+        },
+        "Pivovar Kácov": {
+            "Hubertus Medium": [{"file_name": "042.jpg"}],
+        },
+        "Pivovar Vratislavice": {
+            "Konrád": [{"file_name": "046.jpg"}],
+        },
+        "Zámecký pivovar Břeclav": {
+            "Kanec 11° Hořký": [{"file_name": "063.jpg"}],
+        },
+        "Bohemia Regent": {
+            "Bohemia regent - 10 Světlé + Třináctka Kníže": [{"file_name": "065.jpg"}],
+        },
+        "Pivovar Černá Hora": {
+            "Černá hora - Ležák a Medová 13, Tmavé": [{"file_name": "066.jpg"}],
+        },
+        "Tradiční pivovar v Rakovníku": {
+            "Bakalář": [{"file_name": "068.jpg"}],
+        },
+        "Pivovar Samson": {
+            "Samson": [
+                {"file_name": "070.jpg"},
+                {"file_name": "094.jpg", "variant": "Alternativní foto"},
+            ],
+        },
+        "Pivovar Jihlava": {
+            "Ježek": [{"file_name": "087.jpg"}],
+        },
+        "Pivovar Holba": {
+            "Holba Šerák": [{"file_name": "093.jpg"}],
+        },
+        "Pivovar Volt": {
+            "Volt": [{"file_name": "092.jpg"}],
+        },
+        "Pivovar Nachmelená opice": {
+            "Nachmelená opice 11°": [{"file_name": "096.jpg"}],
+        },
+        "Pivovar Clock": {
+            "Clock": [{"file_name": "097.jpg"}],
+        },
+        "Ostatní CZ": {
+            "Proud": [
+                {"file_name": "006.jpg", "variant": "Černé víčko"},
+                {"file_name": "088.jpg", "variant": "Žluté víčko (soutěžní)"},
+            ],
+            "Pardál": [
+                {"file_name": "028.jpg", "variant": "Červené víčko"},
+                {"file_name": "090.jpg", "variant": "Stříbrné víčko"},
+            ],
+            "Excellent 11°": [{"file_name": "075.jpg"}],
+            "Braník 10°": [{"file_name": "078.jpg"}],
+            "Frisco": [{"file_name": "091.jpg"}],
+        },
+    },
+    "Slovensko": {
+        "Zlatý Bažant": {
+            "Zlatý Bažant": [
+                {"file_name": "080.jpg"},
+                {"file_name": "047.jpg", "variant": "VYHRAJ!"},
+            ]
+        }
+    },
+    "Belgie": {
+        "Brouwerij Lindemans": {
+            "LindemanS kriek (Třešeň)": [{"file_name": "002.jpg"}],
+        },
+        "Kasteel Brouwerij Vanhonsebrouck": {
+            "Kasteel Rubus (Malina)": [{"file_name": "009.jpg"}],
+        },
+        "Brouwerij Van Steenberge": {
+            "Gulden Draak Classic": [{"file_name": "095.jpg"}],
+            "Piraat Triple Hop": [{"file_name": "098.jpg"}],
+            "Piraat": [{"file_name": "099.jpg"}],
+        },
+    },
+    "Německo": {
+        "Brauerei Gebr. Maisel": {
+            "Maisels Weisse": [{"file_name": "040.jpg"}],
+        },
+        "Staatliches Hofbräuhaus": {
+            "Hofbräu Original": [{"file_name": "041.jpg"}],
+        },
+        "Spaten-Franziskaner-Bräu": {
+            "Franziskaner Weissbier": [
+                {"file_name": "057.jpg"},
+                {"file_name": "101.jpg", "variant": "Zlaté víčko"},
+            ],
+        },
+        "Herzoglich Bayerisches Brauhaus Tegernsee": {
+            "Tegernseer Hell": [{"file_name": "103.jpg"}],
+        },
+    },
+    "Rakousko": {
+        "Stieglbrauerei": {
+            "Stiegl Pils": [{"file_name": "023.jpg"}],
+        },
+        "Brauerei Zipf": {
+            "Zipfer Märzen": [{"file_name": "031.jpg"}],
+        },
+        "Brauerei Wieselburg": {
+            "Wieselburger Bier Gold": [{"file_name": "051.jpg"}],
+        },
+        "Brauerei Göss": {
+            "Gösser NaturRadler": [{"file_name": "102.jpg"}],
+        },
+    },
+    "Švédsko": {
+        "IKEA (Krönleins Bryggeri)": {
+            "Öl mörk lager - Černé IKEA pivo": [{"file_name": "013.jpg"}],
+            "Öl ljus lager - IKEA pivo": [{"file_name": "069.jpg"}],
+        }
+    },
+    "Mexiko": {
+        "Grupo Modelo": {
+            "Corona Extra": [{"file_name": "021.jpg"}],
+        }
+    },
+    "Itálie": {
+        "Birra Peroni": {
+            "Peroni": [{"file_name": "053.jpg"}],
+        }
+    },
+    "Nizozemsko": {
+        "Heineken": {
+            "Heineken": [{"file_name": "054.jpg"}],
+        }
+    },
+    "Ukrajina": {
+        "Umanpivo": {
+            "Sofiivske": [{"file_name": "100.jpg"}],
+        }
+    },
+    "Mezinárodní / Ostatní": {
+        "Desperados": {
+            "Desperados": [{"file_name": "074.jpg"}],
+        },
+        "Argus": {
+            "Argus premium 11°, Max X, Bornem": [{"file_name": "001.jpg"}],
+        },
+    },
 }
